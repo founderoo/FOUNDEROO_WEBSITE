f29bbf4539d37b56cdfba5a07a8b69625a6dc311
founderoo <founderocircle@gmail.com>
2025-05-31 19:14:58 +0530

    Created script.js
    this is the file which provides the toggling or the  light to dark mode effect

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("change", () => {
+  document.body.classList.toggle("dark-mode", toggle.checked);
+});
